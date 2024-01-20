30869a56c273790ff68f8dbcdb61b45763b43fad
SRINU <vasu_anand01@yahoo.co.in>
2024-01-20 09:18:51 +0530

    123123

--- main.tf
+++ main.tf
@@ -53,9 +53,8 @@ resource "aws_iam_instance_profile" "instance_profile" {
   role = aws_iam_role.role.name
 }
 
-resource "aws_iam_policy_attachment" "policy-attach" {
-#  name       = "${var.component}-$(var.env)-ec2-role"
-  roles      = [aws_iam_role.role.name]
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  role       = aws_iam_role.role.name
   policy_arn = aws_iam_policy.policy.arn
 }
 
